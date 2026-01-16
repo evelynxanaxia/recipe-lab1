90b96c797671266c0a9b33b281e3f743a0dbbc37
evelynxanaxia <evelynxanixa@icloud.com>
2026-01-16 03:30:18 +0000

    adding recipe.py file

---
+++ recipe.py
@@ -0,0 +1,13 @@
+# %%
+# Recipe step by step instructions to set up a Python environment in GitHub Codespaces
+# 1. Go to GitHub and create a new repository 
+# 2. You should see a grey button that says create a codespace, click it
+# 3. That should take you to VS code online and after it is loaded, click on the three lines on the left side corner and click “Open VS code on desktop”
+# 4. It should prompt you to log into GitHub if you haven’t already, then continue to connect code space to the one you just created 
+# 5. Open terminal and create a virtual environment (python3 -m venv “name of virtual environment”)
+# 6. Activate the environment (source “name of virtual environment”/bin/activate)
+# 7. Add a file to the folder by clicking the small emoticon with the plus size button, name your file this should be the format > “name of file”.py
+# 8. Go to the extension emoticon on the lefthand and click it, you should see a dropdown that says “codespace : miniature meme…” (This is going to allow your file to be interactive) 
+# 9. Click on the cloud on the dropdown and add all the extensions that say python and Jupyter, this will make your file interactive
+# 10. Now add what you need to the .py file, in this case that is everything I added in the recipe.py file
+# 11. After you are done with everything push and commit everything to GitHub through the terminal (1. git add .   2. git commit -m “changes made in file”    3. git push origin)
